aaee77f4c2fb340e9c18319448c679cff6f70ee2
Marvzek <Marvzek@raczek.me>
2022-08-21 00:30:03 +0200

    progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
 
 const int EarthRadius = 6371000;
 const float EssingenLatitudeDeg = 48.7907211;
@@ -18,13 +20,57 @@ float DeltaLambda = 0.0;
 
 float a, c, Distance;
 float y, x, Theta, Bearing;
+
+TinyGPSPlus gps;
+SoftwareSerial SerialGPS(D7, D6); // RX, TX
+
+float Latitude, Longitude;
+int year, month, date, hour, minute, second;
+String DateString, TimeString, LatitudeString, LongitudeString;
+
+float calcDistance();
+float calcBearing();
+
+int readGPS();
+
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Booting");
+
+  SerialGPS.begin(9600);
 }
 
 void loop()
+{
+
+  while (SerialGPS.available() > 0)
+    gps.encode(SerialGPS.read());
+
+  if (gps.location.isUpdated())
+  {
+    Serial.print("LAT=");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print("LNG=");
+    Serial.print(gps.location.lng(), 6);
+    Serial.print("Sat=");
+    Serial.print(gps.satellites.value());
+    Serial.print("Distance=");
+    double distanceKm =
+        gps.distanceBetween(gps.location.lat(), gps.location.lng(), EssingenLatitudeDeg, EssingenLongitudeDeg) / 1000.0;
+    double courseTo =
+        gps.courseTo(gps.location.lat(), gps.location.lng(), EssingenLatitudeDeg, EssingenLongitudeDeg);
+    Serial.print("Distance (km) to Eiffel Tower: ");
+    Serial.print(distanceKm);
+    Serial.print("Course to Eiffel Tower: ");
+    Serial.println(courseTo);
+
+    // readGPS();
+    delay(1000);
+  }
+}
+
+float calcDistance()
 {
   // put your main code here, to run repeatedly:
 
@@ -37,13 +83,36 @@ void loop()
   Distance = EarthRadius * c; // in metres
 
   Serial.println((String) "Distance=" + Distance);
+  return 0.0;
+}
+
+float calcBearing()
+{
+  // 	θ = atan2( sin Δλ ⋅ cos φ2 , cos φ1 ⋅ sin φ2 − sin φ1 ⋅ cos φ2 ⋅ cos Δλ )
   // phi = latitude
   // lambda = longitude
 
   y = sin(CurrentPointLongitudeRad - EssingenLongitudeRad) * cos(CurrentPointLatitudeRad);
   x = cos(EssingenLatitudeRad) * sin(CurrentPointLatitudeRad) - sin(EssingenLatitudeRad) * cos(CurrentPointLatitudeRad) * cos(CurrentPointLongitudeRad - EssingenLongitudeRad);
   Theta = atan2(y, x);
-  Bearing = fmodf((Theta * 180.0 / PI + 360.0),360) ; // in degrees
+  Bearing = fmodf((Theta * 180.0 / PI + 360.0), 360); // in degrees
   Serial.println((String) "Bearing=" + Bearing);
-  delay(1000);
+  return 0.0;
+}
+
+int readGPS()
+{
+  //$GPGGA,214839.00,4826.24617,N,01003.12259,E,1,06,3.11,438.3,M,47.6,M,,*54
+  //$
+
+  char msg[300];
+  bool continueReading = true;
+
+  int writePosition = 0;
+
+  for (int i = 0; i < 1000; i++)
+  {
+    Serial.write(SerialGPS.read());
+  }
+  return 1;
 }
